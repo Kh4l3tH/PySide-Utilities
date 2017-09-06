acbe62f3ab71434bebfee729a70bf95b42caac2f
Kh4l3tH <werther.b@gmail.com>
2017-09-06 13:37:17 +0200

    Added method __len__() to SignalList

--- signallist.py
+++ signallist.py
@@ -8,6 +8,9 @@ class SignalList(QtCore.QObject):
         super().__init__()
         self.list = list()
 
+    def __len__(self):
+        return self.list.__len__()
+
     def append(self, x):
         self.list.append(x)
         self.changed.emit()

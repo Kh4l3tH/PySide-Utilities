61aa7d44442b4fc7eb903e1ba7410fc6fb45f07a
Kh4l3tH <werther.b@gmail.com>
2017-09-06 18:00:28 +0200

    Added SignalDictModel

---
+++ signaldictmodel.py
@@ -0,0 +1,17 @@
+from PySide import QtCore
+
+
+class SignalDictModel(QtCore.QAbstractListModel):
+    def __init__(self, dict):
+        super().__init__()
+        self.dict = dict
+        self.dict.changed.connect(self.layoutChanged)
+
+    def rowCount(self, parent):
+        return len(self.dict)
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            return list(self.dict.keys())[index.row()]
+        elif role == QtCore.Qt.UserRole:
+            return list(self.dict.values())[index.row()]

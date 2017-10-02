f1a14d0272531c8f701eb48b85f597ba6ebaff99
Kh4l3tH <werther.b@gmail.com>
2017-10-02 14:23:41 +0200

    Added OrderedSignalDict

---
+++ orderedsignaldict.py
@@ -0,0 +1,42 @@
+import collections
+import signal
+
+
+class OrderedSignalDict(collections.OrderedDict):
+    def __init__(self, *args, **kwargs):
+        self.changed = signal.Signal()
+        super().__init__(*args, **kwargs)
+
+    def __delitem__(self, *args):
+        super().__delitem__(*args)
+        self.changed.emit()
+
+    def __setitem__(self, *args):
+        super().__setitem__(*args)
+        self.changed.emit()
+
+    def clear(self):
+        super().clear()
+        self.changed.emit()
+
+    def move_to_end(self, *args):
+        super().move_to_end(*args)
+        self.changed.emit()
+
+    def pop(self, *args):
+        value = super().pop(*args)
+        self.changed.emit()
+        return value
+
+    def popitem(self, *args):
+        value = super().popitem(*args)
+        self.changed.emit()
+        return value
+
+    def setdefault(self, *args):
+        super().setdefault(*args)
+        self.changed.emit()
+
+    def update(self, *args):
+        super().update(*args)
+        self.changed.emit()
